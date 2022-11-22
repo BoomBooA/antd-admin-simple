738eec67ca58b40433e86b056065d7facf8a3dfa
BoomBooA <8898141+BoomBooA@users.noreply.github.com>
2022-11-22 17:27:47 +0800

    fix: inject i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
+import './locales'
 import './App.css'
+
 
 function App() {
   return (
