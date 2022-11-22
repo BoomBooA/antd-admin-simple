3b55ec60e1efd6a700fb422042b0324a6398c303
BoomBooA <8898141+BoomBooA@users.noreply.github.com>
2022-11-22 15:28:13 +0800

    feat: add avatar dropdown

--- src/components/Root/index.tsx
+++ src/components/Root/index.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
-import { Layout, Menu } from 'antd'
+import {
+  Avatar,
+  Dropdown,
+  Layout,
+  Menu
+} from 'antd'
+import type { MenuProps } from 'antd'
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
   UploadOutlined,
   UserOutlined,
-  VideoCameraOutlined
+  VideoCameraOutlined,
+  RightSquareOutlined,
+  SettingOutlined,
+  LockOutlined, GlobalOutlined
 } from '@ant-design/icons'
 import { COPYRIGHT } from '@/config'
 
@@ -14,6 +23,38 @@ const { Header, Footer, Sider, Content } = Layout
 
 const Root: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false)
+
+  const userDropDownMenus: MenuProps['items'] = [
+    {
+      key: 'settings',
+      label: 'Settings',
+      icon: <SettingOutlined />
+    },
+    {
+      key: 'password',
+      label: 'Password',
+      icon: <LockOutlined />
+    },
+    {
+      type: 'divider'
+    },
+    {
+      key: 'logout',
+      label: 'Logout',
+      icon: <RightSquareOutlined />
+    }
+  ]
+
+  const i18nDropDownMenus: MenuProps['items'] = [
+    {
+      key: 'en-US',
+      label: 'English'
+    },
+    {
+      key: 'zh-cn',
+      label: '简体中文'
+    }
+  ]
 
   return (
     <Layout>
@@ -48,14 +89,42 @@ const Root: React.FC = () => {
         />
       </Sider>
       <Layout className='site-layout'>
-        <Header
-          className='!bg-white'
-          style={{ padding: 0 }}
-        >
-          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: 'trigger text-lg p-4',
-            onClick: () => setCollapsed(!collapsed)
-          })}
+        <Header className='!bg-white !p-0'>
+          <div className='flex items-start justify-between'>
+            <div className='px-6'>
+              {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                className: 'trigger text-lg',
+                onClick: () => setCollapsed(!collapsed)
+              })}
+            </div>
+            <div className='flex items-center'>
+              <Dropdown
+                className='cursor-pointer'
+                overlayClassName='w-40'
+                menu={{
+                  items: userDropDownMenus
+                }}
+              >
+                <div className='flex items-center'>
+                  <Avatar
+                    size='small'
+                    shape='square'
+                    icon={<UserOutlined />}
+                  />
+                  <span className='px-2'>Admin</span>
+                </div>
+              </Dropdown>
+              <Dropdown
+                className='cursor-pointer mx-2 pr-4'
+                overlayClassName='w-40'
+                menu={{
+                  items: i18nDropDownMenus
+                }}
+              >
+                <GlobalOutlined />
+              </Dropdown>
+            </div>
+          </div>
         </Header>
         <Content className='m-6 mb-0'>
           <div className='container h-full'>
