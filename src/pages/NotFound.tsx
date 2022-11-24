21366448538689629a5bd4c2db6288df90a2f4ad
BoomBooA <8898141+BoomBooA@users.noreply.github.com>
2022-11-24 16:13:20 +0800

    fix: descript not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,18 +1,35 @@
 import React from 'react'
-import { useRouteError } from 'react-router-dom'
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  fa4,
+  fa0
+} from '@fortawesome/free-solid-svg-icons'
 
 const NotFound: React.FC = () => {
-  const error = useRouteError()
+  const renderNumber = (icon: IconDefinition) => {
+    return (
+      <FontAwesomeIcon
+        className='text-6xl m-2'
+        icon={icon}
+      />
+    )
+  }
 
   return (
-    <div className='container'>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='my-4'>
+        {renderNumber(fa4)}
+        {renderNumber(fa0)}
+        {renderNumber(fa4)}
+      </div>
+      <div className='text-xl text-center mb-4'>
+        <p>Page not found</p>
+        <p>Oops! The Page is missing...</p>
+      </div>
     </div>
   )
 }
 
 export default NotFound
+
