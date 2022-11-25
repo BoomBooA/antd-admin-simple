1a15da27b1739c7413a13da380a8bafffad9e4e4
BoomBooA <8898141+BoomBooA@users.noreply.github.com>
2022-11-25 15:43:35 +0800

    fix: init menus

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,13 +1,18 @@
+import { RouteObject } from 'react-router-dom'
 import { createSlice } from '@reduxjs/toolkit'
+import { ItemType } from 'antd/es/menu/hooks/useItems'
+import { isEmpty, map } from 'lodash'
 import router from '@/router'
-
-console.log('router', router)
 
 export interface AppInitialState {
   collapsed: boolean
+  menus: ItemType[]
 }
 
-function initState() {
+/**
+ * read localstorage saved data
+ */
+function initLocalStorage() {
   return Object.keys(localStorage).reduce((previous, key) => {
     return {
       ...previous,
@@ -16,9 +21,47 @@ function initState() {
   }, {})
 }
 
+/**
+ * iteration reading menus
+ * @param menus
+ * @param prevPath
+ */
+function iterationMenus(menus?: RouteObject[], prevPath?: string): ItemType[] {
+  return map(menus, item => {
+    if (!item.path) {
+      return null
+    }
+
+    const label = prevPath ? `page.${prevPath}.${item.path}` : `page.${item.path}`
+
+    return {
+      label,
+      icon: '',
+      key: prevPath ? `${prevPath}-${item.path}` : item.path,
+      children: isEmpty(item.children) ? null : [
+        ...iterationMenus(item.children, item.path)
+      ]
+    }
+  }).filter(item => item)
+}
+
+function initMenus() {
+  const pages = router[0].children
+  return iterationMenus(pages)
+}
+
+function initAppState() {
+  return {
+    ...initLocalStorage(),
+    menus: [
+      ...initMenus()
+    ]
+  }
+}
+
 export const appSlice = createSlice({
   name: 'app',
-  initialState: initState(),
+  initialState: initAppState(),
   reducers: {
     update(state, action) {
       const { key, value } = action.payload
